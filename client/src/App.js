89a1e61b600d3c95bdda09f1931aef05081d820a
mnekx <karimnembac@gmail.com>
2022-05-31 12:15:31 +0300

    initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,22 @@
+import logo from './logo.svg';
 import './App.css';
-import {
-  Route, Routes, Outlet, NavLink,
-} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <nav className="d-flex wrapper">
-        <div className="logo"><a href="/home"><h1>COGNITO</h1></a></div>
-        <ul className="nav-links d-flex">
-            <li><NavLink to="/contribute">Contribute</NavLink></li>
-            <li><NavLink to="/signin">Sign in</NavLink></li>
-        </ul>
-      </nav>
-      <header className="App-header d-flex wrapper">
-        <section>
-          <h2>A new way to train for cognitive ability and aptitude.</h2>
-          <p>Cognito.com helps people especially interview candidates to improve their aptitude and cognitive ability through the use of timed logical, numerical reasoning questions among others.</p>
-          <NavLink className="btn btn-primary" to="/signup">Create account</NavLink>
-        </section>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
--- client/src/index.js
+++ client/src/index.js
@@ -1,21 +1,14 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import { BrowserRouter as Router} from 'react-router-dom'
+import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './index.css';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </Router>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
