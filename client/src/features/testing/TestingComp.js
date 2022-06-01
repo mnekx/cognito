85913bc9c103849172f64412d64eb41c58065b63
mnekx <karimnembac@gmail.com>
2022-06-01 14:19:48 +0300

    Create TestingComp.js

---
+++ client/src/features/testing/TestingComp.js
@@ -0,0 +1,21 @@
+import styles from './testing.module.css';
+
+const Testing = () => {
+    const prolemsData = [
+        {id: 'someid', description: 'qn1', choices: [1, 2, 3, 4], duration: 120},
+        {id: 'someid', description: 'qn1', choices: [1, 2, 3, 4], duration: 120},
+        {id: 'someid', description: 'qn1', choices: [1, 2, 3, 4], duration: 120},
+        {id: 'someid', description: 'qn1', choices: [1, 2, 3, 4], duration: 120},
+        {id: 'someid', description: 'qn1', choices: [1, 2, 3, 4], duration: 120},
+        {id: 'someid', description: 'qn1', choices: [1, 2, 3, 4], duration: 120},
+        {id: 'someid', description: 'qn1', choices: [1, 2, 3, 4], duration: 120},
+        {id: 'someid', description: 'qn1', choices: [1, 2, 3, 4], duration: 120},
+        {id: 'someid', description: 'qn1', choices: [1, 2, 3, 4], duration: 120},
+        {id: 'someid', description: 'qn1', choices: [1, 2, 3, 4], duration: 120},
+        {id: 'someid', description: 'qn1', choices: [1, 2, 3, 4], duration: 120},
+        {id: 'someid', description: 'qn1', choices: [1, 2, 3, 4], duration: 120},
+    ]
+    return <div className={styles.Wrapper}>THIS IS TESTING</div>
+}
+
+export default Testing
