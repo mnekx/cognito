e11a6a110f152684dbbada7be0f27c704b0a9a0a
mnekx <karimnembac@gmail.com>
2022-06-01 14:19:52 +0300

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,28 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SignedoutHome from "./features/signed-out-home/SignedoutHomeComp";
+import SignedinHome from "./features/signed-in-home/SignedinHomeComp";
+import Testing from "./features/testing/TestingComp";
+import Contribution from "./features/contribution/ContributionComp";
+import ProblemList from "./features/problem-list/ProblemListComp";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />}>
+            <Route index element={<SignedoutHome />}></Route>
+            <Route path='user/home' element={<SignedinHome />}>
+              <Route path='take-test' element={<Testing />}></Route>
+              <Route path='contribute' element={<Contribution />}></Route>
+              <Route path="problems" element={<ProblemList/>}></Route>
+            </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
