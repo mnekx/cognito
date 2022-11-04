f432688f0a5ca4d41bea9ca4766b03387a2079a6
mnekx <karimnembac@gmail.com>
2022-11-04 12:34:20 +0300

    Update HeaderComp.js

--- client/src/features/header/HeaderComp.js
+++ client/src/features/header/HeaderComp.js
@@ -1,7 +1,7 @@
 import styles from './Header.module.css';
 
 const Header = ({textInside}) => <header className={styles.AppHeader}>
-{textInside}
+<h1 className="text-3xl font-bold underline">{textInside}</h1>
 </header>
 
 export default Header
