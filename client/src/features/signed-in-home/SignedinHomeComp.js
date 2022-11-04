dd92933cd2636152a5528d28a0c393acab20428c
mnekx <karimnembac@gmail.com>
2022-11-04 12:34:25 +0300

    Update SignedinHomeComp.js

--- client/src/features/signed-in-home/SignedinHomeComp.js
+++ client/src/features/signed-in-home/SignedinHomeComp.js
@@ -1,10 +1,9 @@
 import Navbar from "../navbar/NavbarComp"
-import Header from "../header/HeaderComp"
 import Main from "../main/MainComp";
 import styles from './in.module.css';
 import { Outlet } from "react-router-dom";
 
 const SignedinHome = () => <div className={styles.Wrapper}><Navbar/>
-<Header textInside='SIGNEDIN HOME'/>
-<Main> <Outlet/> </Main></div>
+
+<Main style={{height: '90vh'}}> <Outlet/> </Main></div>
 export default SignedinHome
