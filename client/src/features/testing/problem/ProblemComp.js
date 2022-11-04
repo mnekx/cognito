d22fa6698ee8786ee7333d6e0ec55e428d131959
mnekx <karimnembac@gmail.com>
2022-11-04 12:34:29 +0300

    Update ProblemComp.js

--- client/src/features/testing/problem/ProblemComp.js
+++ client/src/features/testing/problem/ProblemComp.js
@@ -1,24 +1,41 @@
 import styles from './problem.module.css';
 import utilityStyles from '../../../utility.module.css';
-import {FaArrowCircleRight} from 'react-icons/fa';
+import { FaArrowCircleRight } from 'react-icons/fa';
 
-const Problem = ({data, handleNext}) => {
-return (
-    <section className={`${styles.Wrapper} ${utilityStyles.DFlex}`}>
-        <h2 className={styles.Description}>{data.description}</h2 >
+const Problem = ({ data, handleNext }) => {
+
+  return (
+      <section className={`${styles.Wrapper} ${utilityStyles.DFlex}`}>
+        <h2 className={styles.Description}>{data.description}</h2>
         <div className={`${styles.Choices} ${utilityStyles.DFlex}`}>
-            {data.multiple?<h2>Select all that are correct!</h2>: <h2>Select one!</h2>}
-            {data.choices.map(choice => {
-                return (
-                    <label className={`${styles.LabelControl} ${utilityStyles.DBlock}`}>
-                        <input type={data.multiple?'checkbox': 'radio'} value={choice} name={data.id}/>
-                        {choice}
-                    </label>
-                )
-            })}
-            <FaArrowCircleRight size='2em' className={styles.Next} onClick={()=>handleNext()}/>
+          {data.multiple ? (
+            <h2>Select all that are correct!</h2>
+          ) : (
+            <h2>Select one!</h2>
+          )}
+          {data.choices.map((choice) => {
+            return (
+              <label
+              key={choice}
+                className={`${styles.LabelControl} ${utilityStyles.DBlock}`}
+              >
+                <input
+                  type={data.multiple ? 'checkbox' : 'radio'}
+                  value={choice}
+                  name={data.id}
+                />
+                {choice}
+              </label>
+            );
+          })}
+          <FaArrowCircleRight
+            size='2em'
+            className={styles.Next}
+            onClick={() => handleNext()}
+          />
         </div>
-    </section>
-)}
+      </section>
+  );
+};
 
 export default Problem;
